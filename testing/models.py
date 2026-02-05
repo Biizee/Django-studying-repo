80850a9d51199d901c9afd40c1b17fc27107e1b3
Heorhii Ponomarenko <ge.ponomarenko@gmail.com>
2026-02-05 15:36:56 +0100

    Added new model

--- testing/models.py
+++ testing/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
 # Create your models here.
+
+class LibraryBook(models.Model):
+    title = models.CharField(max_length=312)
+    author = models.CharField(max_length=200)
+    isbn = models.CharField(max_length=26)
+    available = models.BooleanField()
+
+    def __str__(self):
+        return f"{self.title} - {self.author}"
+    
+    class Meta:
+        verbose_name = "Book"
+        verbose_name_plural = "Books"
+        ordering = ["author"]
+        indexes = [
+            models.Index(fields=["isbn"])
+        ]
